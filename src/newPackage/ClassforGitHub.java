22430309445be52d447e0b0410f3a3f63534b022
qararander <qararander@hotmail.com>
2022-06-01 18:19:36 -0700

    added a new class

---
+++ src/newPackage/ClassforGitHub.java
@@ -0,0 +1,12 @@
+package newPackage;
+
+public class ClassforGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, I am studentforever!");
+		
+	}
+
+}
